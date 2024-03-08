0750ab04d97cefc0e4f59359b9a27bacfe2866ba
prachisv04 <prachisvavhal@gmail.com>
2024-03-08 12:45:51 +0530

    navbar corrected

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,15 +5,6 @@ export default function LeftSidebar(props) {
     <>
    
 <div class="vertical-nav bg-white" id="sidebar">
-  {/* <div class="py-4 px-3 mb-4 bg-light">
-    <div class="media d-flex align-items-center"><img src="https://bootstrapious.com/i/snippets/sn-v-nav/avatar.png" alt="..." width="65" class="mr-3 rounded-circle img-thumbnail shadow-sm" />
-      <div class="media-body">
-        <h4 class="m-0">Prachi Vavhal</h4>
-        <p class="font-weight-light text-muted mb-0">Full Stack Developer</p>
-      </div>
-    </div>
-  </div> */}
-
   <p class="text-gray font-weight-bold text-uppercase py-3 px-3 small pb-4 mb-0">Introduction</p>
 
   <ul class="nav flex-column bg-white mb-0">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,32 +2,38 @@ export default function Navbar(props) {
     return (
      <>
      
-     <nav id="top-navigation" class="menuNav">
+     <nav>
       <div class="navbar navbar-expand-lg pt-4 " >
-        <div class="d-flex justify-content-between">
         <a href="/" class="brand text-decoration-none d-block d-lg-none fw-bold fs-1"> UPSKILL </a>
-        
-        <button id="navbarCollapse" type="button" class="btn btn-light bg-white  rounded-pill shadow-sm px-4 mb-4 float-right"><i class="fa fa-bars mr-2"></i><small class="text-uppercase font-weight-bold">MENU</small></button>
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        </div>
-        <div class="container-fluid">
-        
-    <div  id="menu">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul id="nav-length" class="navbar-nav justify-content-between border-top border-2 text-center">
+             <div class="py-2 px-3 mb-4 bg-light d-lg-none" id="profile">
+              <div class="media d-flex align-items-center"><img src="https://bootstrapious.com/i/snippets/sn-v-nav/avatar.png" alt="..." width="65" class="mr-3 rounded-circle img-thumbnail shadow-sm" />
+                <div class="media-body">
+                  <h4 class="m-0 text-dark">Prachi Vavhal</h4>
+                  <p class="font-weight-light text-muted mb-0">Full Stack Developer</p>
+                </div>
+              </div>
+            </div> 
               <li class="nav-item">
-                <a href="/" class="nav-link border-hover py-3 text-white">Home</a>
+                <a href="/" class="nav-link border-hover ">Home</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link border-hover py-3 text-white active">Courses</a>
+                <a href="/" class="nav-link border-hover active">Courses</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link border-hover py-3 text-white">Practice</a>
+                <a href="/" class="nav-link border-hover ">Practice</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link border-hover py-3 text-white">About</a>
+                <a href="/" class="nav-link border-hover ">About</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link border-hover py-3 text-white">Contact</a>
+                <a href="/" class="nav-link border-hover ">Contact</a>
               </li>
               <li class="nav-item">
                 <a href="/" id="sign-in" class="nav-link my-2 px-4 text-white">
@@ -38,7 +44,7 @@ export default function Navbar(props) {
             </ul>
           </div>
         </div>
-      </div>
+      
      </nav>
      </>
     );
